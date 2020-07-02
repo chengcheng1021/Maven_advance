c52022e932a8e29c6984f6bde2c0786fa73b4208
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-02 16:30:07 +0800

    service层代码编写

---
+++ maven_day02_1/src/main/java/com/cc/service/impl/ItemsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.cc.service.impl;
+
+import com.cc.dao.ItemsDao;
+import com.cc.domain.Items;
+import com.cc.service.ItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemsServiceImpl implements ItemsService {
+
+    @Autowired
+    private ItemsDao itemsDao;
+
+    public Items findById(Integer id) {
+        return itemsDao.findById(id);
+    }
+}
--- maven_day02_1/src/test/java/com/cc/test/ItemsTest.java
+++ maven_day02_1/src/test/java/com/cc/test/ItemsTest.java
@@ -2,12 +2,16 @@ package com.cc.test;
 
 import com.cc.dao.ItemsDao;
 import com.cc.domain.Items;
+import com.cc.service.ItemsService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ItemsTest {
 
+    /**
+     * dao 测试
+     */
     @Test
     public void findById() {
         //获取 spring 容器
@@ -16,6 +20,19 @@ public class ItemsTest {
         ItemsDao itemsDao = ac.getBean(ItemsDao.class);
         //调用方法
         Items items = itemsDao.findById(1);
+        System.out.println(items.getName());
+    }
+
+    /**
+     * service 测试
+     */
+    @Test
+    public void findById2() {
+        //获取 spring 容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ItemsService itemsService = ac.getBean(ItemsService.class);
+        //调用方法
+        Items items = itemsService.findById(1);
         System.out.println(items.getName());
     }
 }
