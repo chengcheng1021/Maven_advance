fd489f1dffe41cd3eb0c503cd73e29e2bd2de553
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-20 10:58:53 +0800

    servletContext对象

---
+++ javaweb-02-servlet/servlet-02/src/main/java/com/cc/servlet/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.cc.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello");
+
+        ServletContext servletContext = this.getServletContext();
+        String name = "程成";
+        servletContext.setAttribute("username", name);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+}
