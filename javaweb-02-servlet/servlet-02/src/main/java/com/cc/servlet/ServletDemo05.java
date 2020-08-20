aeaef91d3175cf0399287df5d6a624034f30f729
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-20 13:39:59 +0800

    servletContext读取properties

---
+++ javaweb-02-servlet/servlet-02/src/main/java/com/cc/servlet/ServletDemo05.java
@@ -0,0 +1,31 @@
+package com.cc.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ServletDemo05 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+
+        InputStream resourceAsStream = servletContext.getResourceAsStream("/WEB-INF/classes/db.properties");
+        Properties properties = new Properties();
+        properties.load(resourceAsStream);
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+
+        resp.getWriter().println(username + ":" + password);
+    }
+}
